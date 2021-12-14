c39ab54c7b771cff5d8c0c445e5078176b6797a2
maplepy <sergienkoandrew9922@gmail.com>
2021-12-14 17:19:05 +0200

    fix bugs

--- HT_08/task1/ATM.py
+++ HT_08/task1/ATM.py
@@ -33,6 +33,7 @@ def transaction(user, oper, num):
             print(f"Виконано. Баланс: {new_balance}.")
             if oper == "-":
                 print("Готівка: \n" + format_cash(return_cash))
+                update_all_cash(return_cash, "-")
         message = f'"status": "{status}", "operation": "{operations_message[oper]}", "balance": {balance}'
     dt.log_transaction(user, message)
 
@@ -61,9 +62,9 @@ def update_all_cash(banknotes: list, oper):
         for banknote_atm in banknotes_atm:
             if banknote[0] == banknote_atm[0]:
                 flag = True
-                banknote[1] = operation(oper, banknote_atm[1], banknote[1])
+                banknote_atm[1] = operation(oper, banknote_atm[1], banknote[1])
         if not flag and oper == "+":
-            banknotes.append([banknote, operation(oper, 0, banknote[1])])
+            banknotes_atm.append([banknote[0], operation(oper, 0, banknote[1])])
     dt.set_cash(banknotes_atm)
 
 
@@ -74,7 +75,7 @@ def format_cash(cash):
     return result_str[:-2]
 
 
-def menu(user, number_operation):
+def user_menu(user, number_operation):
     if number_operation == "1":
         print("Ваш баланс:", dt.get_balance(user))
     elif number_operation == "2":
@@ -83,13 +84,31 @@ def menu(user, number_operation):
     elif number_operation == "3":
         num = int(input("Введіть кількість: "))
         transaction(user, "+", num)
-    elif number_operation == "4":
-        cash_str = input("Введіть значення - <купюра: кількість>: ")
-        cash = cash_str.split(': ')
-        cash = list(map(int, cash))
-        update_all_cash([cash], "+")
     else:
         return "Exit"
+
+
+def collector_menu(user, number_operation):
+    if number_operation == "1":
+        print("Купюри:", format_cash(dt.get_banknotes()))
+    elif number_operation == "2":
+        num_str = input("Введіть дані наступним чином: <<banknote1: value1>, <banknoteN: valueN>>: ")
+        num = num_str.split(", ")
+        if is_valid_banknotes(num):
+            num = [list(map(int, x.split(": "))) for x in num]
+            update_all_cash(num, "+")
+            print("Купюри внесено. Наявні купюри: ", format_cash(dt.get_banknotes()))
+            message = f'"status": "OK", "operation": "Внесення купюр"'
+            dt.log_transaction(user, message)
+        else:
+            print("Невірно введені данні. Спробуйте ще раз.")
+    else:
+        return "Exit"
+
+
+def num_selection(message):
+    print(message)
+    return input("Виберіть дію: ")
 
 
 def login(username, password):
@@ -100,14 +119,29 @@ def login(username, password):
 
 
 def start():
+    user_menu_message = "=================================\n" \
+                        "1. Подивитись баланс\n" \
+                        "2. Зняти кошти\n" \
+                        "3. Поповнити кошти\n" \
+                        "інше. Вийти\n"
+    collector_menu_message = "=================================\n" \
+                             "1. Подивитись список наявних купюр\n" \
+                             "2. Заправити готівку\n" \
+                             "інше. Вийти\n"
     user = False
     count = 2
+    user_type = None
     while count >= 0:
         username = input("Введіть логін: ")
         password = input("Введіть пароль: ")
         user = login(username, password)
         if user:
-            print("Ви зайшли в систему")
+            types = {
+                'collector': 'Колектор',
+                'user': 'Користувач'
+            }
+            user_type = dt.get_user_type(user)
+            print("Ви зайшли в систему як", types[user_type])
             count = 0
         else:
             if count == 0:
@@ -116,17 +150,31 @@ def start():
             print(f"Невірний логін або пароль. Осталось спроб: ", count)
         count -= 1
 
+    if user_type == "collector":
+        menu = collector_menu
+        message = collector_menu_message
+    else:
+        menu = user_menu
+        message = user_menu_message
+
     while True:
-        print("=================================\n"
-              "1. Подивитись баланс\n"
-              "2. Зняти кошти\n"
-              "3. Поповнити кошти\n"
-              "4. Повонити готівку\n"
-              "інше. Вийти\n",)
-        num_operation = input("Виберіть дію:")
+        num_operation = num_selection(message)
         result = menu(user, num_operation)
         if result == "Exit":
             return
+        print()
+
+
+def is_natural_number(num_str: str):
+    return True if num_str.isdigit() and int(num_str) >= 0 else False
+
+
+def is_valid_banknotes(b_list: list):
+    for banknote in b_list:
+        banknote = banknote.split(": ")
+        if not is_natural_number(banknote[0]) or not is_natural_number(banknote[1]):
+            return False
+    return True
 
 
 start()
--- HT_08/task1/data_tools.py
+++ HT_08/task1/data_tools.py
@@ -30,18 +30,18 @@ def log_transaction(user, message):
     date = now.strftime("%d/%m/%Y")
     time = now.strftime("%H:%M:%S")
     log_message = '{' + f'"date": "{date}", "time": "{time}", {message}' + '}\n'
-    write_data(get_path('transactions', user), 'w', log_message)
+    write_data(get_path('transactions', user), 'a', log_message)
 
 
 def get_banknotes():
-    banknotes_str = read_full_data(get_path("amount_cash"))
+    banknotes_str = read_full_data(get_path("amount_cash")).rstrip("\n")
     banknotes = banknotes_str.split("\n")
-    return [[int(x.split(': ')[0]), int(x.split(': ')[1])] for x in banknotes]
+    return sorted([list(map(int, x.split(": "))) for x in banknotes], key=lambda x: x[0], reverse=True)
 
 
 def get_users():
     users = read_full_data("data/users.data").split('\n')
-    users = [[user.split(', ')[0], user.split(', ')[1]] for user in users]
+    users = [user.split(', ')[:-1] for user in users]
     return users
 
 
@@ -49,4 +49,12 @@ def set_cash(cash: list):
     cash_str = ""
     for banknote in cash:
         cash_str += f"{banknote[0]}: {banknote[1]}\n"
-    write_data("data/amount_cash.data", 'w', cash_str)
+    write_data("data/amount_cash.data", 'w', cash_str.rstrip("\n"))
+
+
+def get_user_type(username):
+    users = read_full_data("data/users.data").split('\n')
+    users = [user.split(', ') for user in users]
+    for user in users:
+        if username == user[0]:
+            return user[2]
