bd024101228139b275c0356064ee95182e80cbe9
AndrewSergienko <sergienkoandrew9922@gmail.com>
2022-02-02 11:27:31 +0200

    register admin models

--- HT_19_2/stories/admin.py
+++ HT_19_2/stories/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ask, New, Job, Show
+
+
+@admin.register(Ask)
+class AskAdmin(admin.ModelAdmin):
+    list_display = ['story_id', 'title', 'time', 'by']
+
+
+@admin.register(New)
+class NewAdmin(admin.ModelAdmin):
+    list_display = ['story_id', 'title', 'time', 'by']
+
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ['story_id', 'title', 'time', 'by']
+
+
+@admin.register(Show)
+class ShowAdmin(admin.ModelAdmin):
+    list_display = ['story_id', 'title', 'time', 'by']
