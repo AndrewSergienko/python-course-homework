fb22e6a34ba5eb1bfc5796bc86d0935dc87512c3
AndrewSergienko <sergienkoandrew9922@gmail.com>
2022-02-08 00:32:33 +0200

    add account urls

--- HT_20/HT_20/urls.py
+++ HT_20/HT_20/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/', include('account.urls', namespace='account')),
     path('cart/', include('cart.urls', namespace='cart')),
     path('', include('shop.urls', namespace='shop'))
 ]
